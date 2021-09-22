345dd4362210e7b8c19ef44afe1debf3ba330076
Geliel1 <85584428+Geliel1@users.noreply.github.com>
2021-09-21 23:03:57 -0300

    Finalizando mais um dia de aula!

---
+++ aula02/Exercicio02/script.js
@@ -0,0 +1,11 @@
+var nome = "Geliel"
+
+var sobreNome = "Gustavo"
+
+let idade = 20
+
+let sexo = "Masculino"
+
+const CPF = 13115116480
+
+const TELEFONE = 87981133800
---
+++ aula02/exercicio01/script01.js
@@ -0,0 +1,18 @@
+//variável do tipo Number
+var numero = 10
+    //Variavel do tipo String
+var nome = "Geliel"
+    //variável do tipo Boolean
+var verdadeiro = true
+    //variável do tipo Arrey
+var frutas = ["Abacaxi", "Morango", "uva"]
+    //variavel do tipo Object
+var moto = {
+    Modelo: "Kawasaki Ninja",
+    Cor: "Verde Limão",
+    Ano: 2011
+};
+//variável com o valor Undefined
+var valorNaoDefinido;
+//variável com valor vazio
+var valor_vazio = null
